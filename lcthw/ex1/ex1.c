2f742e2b9aea69802831801271eb86fe55123723
Piyush Mishra <piymis6@gmail.com>
2020-04-12 09:52:28 +0530

    Exercise 2

---
+++ lcthw/ex1/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/* This is a comment */
+
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	//
+	
+	printf("You are %d miles away", distance);
+
+	return 0;
+}
+
